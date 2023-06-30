678ae24f673bc54727a703b4785b87b29c518f23
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2023-06-30 15:39:41 -0300

    ver 1.3

--- react-native.js
+++ react-native.js
@@ -4,7 +4,7 @@ module.exports = {
     "ecmaFeatures": {
       "jsx": true
     },
-    "ecmaVersion": 2018,
+    "ecmaVersion": latest,
     "sourceType": "module"
   },
   extends: ['@react-native-community', 'airbnb-typescript', 'prettier'],
