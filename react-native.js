d33cc25989c52ebcf32d3dc647c288321cb430c5
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2023-07-05 12:01:54 -0300

    fix in [react-native] [prettier] - trailingComma

--- react-native.js
+++ react-native.js
@@ -43,7 +43,7 @@ module.exports = {
         {
           endOfLine: 'auto',
           singleQuote: false,
-          trailingComma: true,
+          trailingComma: "all",
           semi: true,
         },
       ],
