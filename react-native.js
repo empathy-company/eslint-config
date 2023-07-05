0365c9faf4cafec2054406e0a540c92a9879e6b5
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2023-07-05 11:53:17 -0300

    fix react-native

--- react-native.js
+++ react-native.js
@@ -1,24 +1,52 @@
 module.exports = {
-  root: true,
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    },
-    "ecmaVersion": latest,
-    "sourceType": "module"
-  },
-  extends: ['@react-native-community', 'airbnb-typescript', 'prettier'],
-  rules: {
-    'import/extensions': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'no-lone-blocks': 'off',
-    'react-native/no-inline-styles': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: "module",
+      ecmaFeatures: {
+        jsx: true,
       },
+    },
+    env: {
+      browser: true,
+      "react-native/react-native": true,
+    },
+    plugins: ["react", "react-native"],
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "@react-native-community",
+      "airbnb-typescript",
+      "prettier",
     ],
-  },
-};
+    rules: {
+      "react-native/no-unused-styles": 2,
+      "react-native/split-platform-components": 2,
+      "react-native/no-color-literals": 2,
+      "react-native/no-raw-text": 2,
+      "react-native/sort-styles": [
+        "error",
+        "asc",
+        {
+          ignoreClassNames: false,
+          ignoreStyleProperties: false,
+        },
+      ],
+      "import/extensions": "off",
+      "@typescript-eslint/no-use-before-define": "off",
+      "import/no-extraneous-dependencies": "off",
+      "no-lone-blocks": "off",
+      "react-native/no-inline-styles": "off",
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: 'auto',
+          singleQuote: false,
+          trailingComma: true,
+          semi: true,
+        },
+      ],
+    },
+  };
+  
